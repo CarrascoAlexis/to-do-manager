0c96dc71dc95950b7c3e6c12c57a2c124527ce5e
kroko_san <acarrasco@gaming.tech>
2025-10-29 20:26:52 +0100

    Add Vue Router and restructure components
    - Added "vue-router" dependency to package.json and package-lock.json.
    - Moved App.vue to components directory and updated import path in main.ts.
    - Created new Header.vue, Footer.vue, and App.vue components with basic structure.

---
+++ src/components/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+</script>
+
+<template>
+  <Header></Header>
+  <main>
+    Main body
+  </main>
+  <Footer></Footer>
+</template>
+
+<style scoped></style>
